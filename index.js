53f51d27106e99df676d2f598108e024e15484f3
ebo16@vt.edu <ebo16@vt.edu>
2021-11-07 18:30:47 -0500

    added html

--- index.js
+++ index.js
@@ -1,19 +1,242 @@
-//initialize all things at top
+//inititalize the programs
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-//create team array
+//initialize all the team member profiles
+const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-//Manager then inquirer prompt
-// name , id , email, office number
-//push manager to team
+// //generates an html file
+const generateHTML = require("./src/generateHTML.js");
+
+//create empty team array to push all team members too
+const teamMembers = [];
+
+//start of adding a manager by user input
+const addingManager = () => {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the managers name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Enter the managers name");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the managers id?',
+                validate: idInput => {
+                    if (idInput) {
+                        return true;
+                    } else {
+                        console.log("Enter the managers id");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the managers email?',
+                validate: function (email) {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("Enter the managers email");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the managers id?',
+                validate: idInput => {
+                    if (idInput) {
+                        return true;
+                    } else {
+                        console.log("Enter the managers id");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the managers office number?',
+                validate: officeNumberInput => {
+                    if (officeNumberInput) {
+                        return true;
+                    } else {
+                        console.log("Enter the managers id");
+                        return false;
+                    }
+                }
+            },
+        ])
+
+        .then(managerInput => {
+            const { name, id, email, officeNumber } = managerInput;
+            const manager = new Manager(name, id, email, officeNumber);
+
+            //push manager to team
+            teamMembers.push(manager);
+            console.log(manager);
+        })
+};
+
+const addingEmployee = () => {
+    inquirer.prompt(
+        [
+            {
+                type: 'checkbox',
+                name: 'role',
+                message: 'Choose your employees role',
+                choices: ['Engineer', 'Intern']
+            },
+
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the employees name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Enter an employees name");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the employees id?',
+                validate: idInput => {
+                    if (idInput) {
+                        return true;
+                    } else {
+                        console.log("Enter an employees id");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the employees email?',
+                validate: function (email) {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("Enter the employees email");
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'school',
+                message: 'Enter the interns school',
+                when: (input) => input.role === "Intern",
+                validate: schoolInput => {
+                    if (schoolInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's school!")
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the employees github username?',
+                when: (input) => input.role === 'Engineer',
+                validate: githubInput => {
+                    if (githubInput) {
+                        return true;
+                    } else {
+                        console.log("Enter the employee's github username!")
+                    }
+                }
+            },
+
+            {
+                type: 'confirm',
+                name: 'confirmMoreEmployee',
+                message: 'Do you want to add more employees?',
+                default: false
+            }
+        ])
+        .then(employeeData => {
+
+            let { name, id, email, role, github, school, confirmMoreEmployee } = employeeData;
+            let employee;
+
+            if (role === "Intern") {
+                employee = new Intern(name, id, email, school);
+                console.log(employee);
+            } else if (role === "Engineer") {
+                employee = new Engineer(name, id, email, github);
+                console.log(employee);
+            }
+
+            teamMembers.push(employee);
+
+            if (confirmMoreEmployee) {
+                return addingEmployee(teamMembers);
+            } else {
+                return teamMembers;
+            }
+        })
+
+};
+
+// function to generate HTML page file using file system 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+};
 
 
+// addingManager()
+    //.then
+    (addingEmployee())
+    // .then(teamMembers => {
+    //     return generateHTML(teamMembers);
+    // })
+    // .then(pageHTML => {
+    //     return writeFile(pageHTML);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
 
-//inquirer prompt
-//ask to add another team member either intern or engineer
-// name , id , email
-// if statements based on employee type and ask school and guthub name
-//push to team
-
-//push info to html page
 
 
---
+++ src/generateHTML.js
@@ -0,0 +1,130 @@
+// create the manager html
+const generateManager = manager => {
+    return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.name}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+                <li class="list-group-item">Office number: ${manager.OfficeNumber}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+};
+
+// create the html for engineers
+const generateEngineer = engineer => {
+    return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${engineer.name}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.role}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+        </ul>
+    </div>
+</div>
+        `;
+};
+
+// create the html for interns
+const generateIntern = intern => {
+    return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${intern.name}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.role}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+            <li class="list-group-item">School: ${intern.school}</li>
+        </ul>
+    </div>
+</div>
+        `;
+};
+
+generateHTML = (data) => {
+
+    const html = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+
+        // call manager function
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+            html.push(managerCard);
+        }
+
+        // call engineer function
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+            html.push(engineerCard);
+        }
+
+        // call intern function 
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+            html.push(internCard);
+        }
+    }
+
+    const employeeCards = html.join("");
+
+
+    const generateTeam = generateTeamPage(employeeCards)
+    return generateTeam;
+}
+// export function to generate entire page
+
+const generateTeamPage = employeeCards => {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+                ${employeeCards}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `;
+};
+
+module.exports = generateHTML;
